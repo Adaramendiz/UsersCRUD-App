66bd34e8a16428a9b09eef202b1b5192dc52df2f
andres <adaramendiz@gmail.com>
2023-07-17 13:28:59 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,54 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import useFetch from './hooks/useFetch'
-import UserCard from './components/UserCard'
-import FormUser from './components/FormUser'
+import { useEffect, useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import UserCard from "./components/UserCard";
+import FormUser from "./components/FormUser";
 
 function App() {
+  const [updateInfo, setUpdateInfo] = useState();
+  const [closeForm, setCloseForm] = useState(true);
 
-  const [updateInfo, setUpdateInfo] = useState()
-  const [closeForm, setCloseForm] = useState(true)
-  
-  const baseUrl = 'https://users-crud.academlo.tech'
+  const baseUrl = "https://users-crud.academlo.tech";
 
-  const [ user, getAllUsers, createNewUser, deleteUserById, updateUserById ] = useFetch(baseUrl, setCloseForm)
+  const [user, getAllUsers, createNewUser, deleteUserById, updateUserById] =
+    useFetch(baseUrl, setCloseForm);
 
   useEffect(() => {
-    getAllUsers('/users')
-  }, [])
-  
+    getAllUsers("/users");
+  }, []);
+
   const handleOpenForm = () => {
-    setCloseForm(false)
-  }
+    setCloseForm(false);
+  };
 
   return (
     <div>
       <h1>Users</h1>
-      <button onClick={handleOpenForm} className='formuser__btn'>Open Form</button>
+      <button onClick={handleOpenForm} className="user__btn">
+        {" "}
+        + Create new user
+      </button>
       <FormUser
-      createNewUser={createNewUser}
-      updateInfo={updateInfo}
-      updateUserById={updateUserById}
-      setUpdateInfo={setUpdateInfo}
-      closeForm={closeForm}
-      setCloseForm={setCloseForm}
+        createNewUser={createNewUser}
+        updateInfo={updateInfo}
+        updateUserById={updateUserById}
+        setUpdateInfo={setUpdateInfo}
+        closeForm={closeForm}
+        setCloseForm={setCloseForm}
       />
-      <div>
-        {
-          user?.map(user =>  ( 
-            <UserCard
+      <div className="container">
+        {user?.map((user) => (
+          <UserCard
             key={user.id}
             user={user}
             deleteUserById={deleteUserById}
             setUpdateInfo={setUpdateInfo}
             handleOpenForm={handleOpenForm}
-            />
-          ))
-        }
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,38 +1,45 @@
-import './styles/UserCard.css'
+import "./styles/UserCard.css";
 
-const UserCard = ({user, deleteUserById, setUpdateInfo, handleOpenForm}) => {
+const UserCard = ({ user, deleteUserById, setUpdateInfo, handleOpenForm }) => {
+  const handleDelete = () => {
+    deleteUserById("/users/", user.id);
+  };
 
-  const handleDelete = ( ) => {
-    deleteUserById('/users/', user.id)
-  }
-
-  const handleUpdate = ( ) => {
-    setUpdateInfo(user)
-    handleOpenForm()
-  }
+  const handleUpdate = () => {
+    setUpdateInfo(user);
+    handleOpenForm();
+  };
 
   return (
     <article className="article__card">
-      <h2 className='article__card-title' >{user.first_name} {user.last_name}</h2>
-      <hr  />
+      <h2 className="article__card-title">
+        {user.first_name} {user.last_name}
+      </h2>
+      <hr />
       <ul>
-        <li className='article__card-email-birthday'>
-          <span className='article__card-email' >Email </span>
+        <li className="article__card-email-birthday">
+          <span className="article__card-email">Email </span>
           <span>{user.email}</span>
         </li>
-        <li className='article__card-email-birthday'>
-          <span className='article__card-birthday' >Birthday</span>
-          <span> <i className='bx bx-gift'></i> {user.birthday}</span>
+        <li className="article__card-email-birthday">
+          <span className="article__card-birthday">Birthday</span>
+          <span>
+            {" "}
+            <i className="bx bx-gift"></i> {user.birthday}
+          </span>
         </li>
       </ul>
       <hr />
-      <footer className='footer__btn'>
-        <button className='footer__btn-delete' onClick={handleDelete}><i className='bx bx-trash'></i></button>
-        <button className='footer__btn-update' onClick={handleUpdate}><i className='bx bx-pencil'></i></button>
+      <footer className="footer__btn">
+        <button className="footer__btn-delete" onClick={handleDelete}>
+          <i className="bx bx-trash"></i>
+        </button>
+        <button className="footer__btn-update" onClick={handleUpdate}>
+          <i className="bx bx-pencil"></i>
+        </button>
       </footer>
-
     </article>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
